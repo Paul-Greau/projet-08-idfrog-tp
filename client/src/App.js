879ab5c05b92c36903a6012ad86256836091fe70
Paul-Greau <mrgreau@gmail.com>
2022-11-03 12:17:38 +0100

    notfoundpage-v1

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import ProjectCard from "./components/ProjectCard/ProjectCard";
 import ProjectCollect from "./components/ProjectCollect/ProjectCollect";
 import Footer from './components/Footer/Footer'
 import Navigation from "./components/Navigation/Navigation";
+import NotFound from "./components/NotFound/NotFound";
 
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
-      <ProjectCard/>
-      <ProjectCollect/>
+      <NotFound/>
       <Footer/>
     </div>
   );
