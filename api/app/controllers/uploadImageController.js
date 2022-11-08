1e8939ccd00c52dbf3c3809add32be22a3eb2895
Alex <alexandre.legleye@gmail.com>
2022-11-08 12:36:47 +0400

    feat: check if profile is login

--- api/app/controllers/uploadImageController.js
+++ api/app/controllers/uploadImageController.js
@@ -3,19 +3,35 @@ const path = require('path')
 const uploadImageController = {
 
 uploadProjectImage : (req, res) => {
-  const rootDirectory = require('path').resolve('./');
-  const projectId = req.params.id
 
-    let projectImage;
-    let uploadPath;
+  const projectId = req.params.id
+  const profile_id  = Number(req.body.profile_id);
+  
+  console.log(profile_id);
+  
+  if (!profile_id) {
+    const error = new Error(`'profile_id' property is missing`);
+    return res.status(400).json({ message: error.message });
+  }
+  if (!req.session.profile) {
+    const error = new Error(`You must login`);
+    return res.status(401).json({ message: error.message });
+  }	
+  if (profile_id !== req.session.profile.id) {
+    const error = new Error(`You must login before upload an Image`);
+    return res.status(401).json({ message: error.message });
+  }
+
+  const rootDirectory = require('path').resolve('./');
+  
   
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send('No files were uploaded.');
     }
   
     // The name of the input field is used to retrieve the uploaded file
-    projectImage = req.files.projectImage;
-    uploadPath = `${rootDirectory}/data/ProjectsImages/project${projectId}-${projectImage.name}`;
+    const projectImage = req.files.projectImage;
+    const uploadPath = `${rootDirectory}/data/ProjectsImages/project${projectId}-${projectImage.name}`;
 
     // check if file already exist
     if(fs.existsSync(uploadPath)){
