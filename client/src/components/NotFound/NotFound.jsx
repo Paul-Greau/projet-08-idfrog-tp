856b3ade1596c7aaa3c8bb1406a9c980575a6c3b
Paul-Greau <mrgreau@gmail.com>
2022-11-10 09:28:28 +0100

    add span to notfoundpage

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { Grid } from '@mui/material';
 import notfoundImg from "../../assets/images/404 .svg"
 import { Typography } from '@mui/material';
+// CSS
+import palette from '../../assets/styles/_vars.scss';
 
 
 import './notFoundStyles.scss';
@@ -19,7 +21,7 @@ function NotFound() {
             }}
         >
             <Grid item xs={12} sm={12} sx={{ textAlign: 'left', marginBottom: "-40px" }}>
-                <Typography variant="h1" color="" sx={{fontSize: "2rem", fontWeight: "bold"}}>Oups ! La page que vous recherchez semble introuvable</Typography>
+                <Typography variant="h1" color="" sx={{fontSize: "2rem", fontWeight: "bold"}}>Oups ! <span style={{ color: palette.primary }}>La page</span> que vous recherchez<span style={{ color: palette.primary }}>semble introuvable</span></Typography>
             </Grid>
             <Grid item xs={12} sm={12} sx={{ textAlign: 'center', marginBottom:"30px" }}>
                 <img width="70%" src={notfoundImg} className="notfoundpage-logo" alt="Image 404 not found"></img>
