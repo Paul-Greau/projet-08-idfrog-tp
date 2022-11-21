2b415203ab2a0d3123d29c6f9a4edb30b15ef6f1
Alex <alexandre.legleye@gmail.com>
2022-11-21 11:30:25 +0400

    add edit projectprojectForm

--- client/src/components/UI/forms/Utils/Utils.js
+++ client/src/components/UI/forms/Utils/Utils.js
@@ -14,3 +14,20 @@ export const handleSubmitProfil = (response, validStatus) => {
     showMessage: true
   }
 } 
+
+export const handlePatchProject = (response, validStatus) => {
+  if (response.status === validStatus){
+    return {
+      alertStyle: "success",
+      errorStatus: null,
+      message: "Projet mis à jour",
+      showMessage: true
+    }
+  }
+  return {
+    alertStyle: "error",
+    errorStatus: response.status,
+    message: response.data.message,
+    showMessage: true
+  }
+} 
