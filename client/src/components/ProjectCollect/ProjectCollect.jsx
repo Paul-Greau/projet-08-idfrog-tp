ea8b687b44666e2a61697885a4193aaff98af2a9
Paul-Greau <mrgreau@gmail.com>
2022-12-19 17:56:54 +0100

    test

--- client/src/components/ProjectCollect/ProjectCollect.jsx
+++ client/src/components/ProjectCollect/ProjectCollect.jsx
@@ -46,7 +46,7 @@ function ProjectCollect({
   contributions,
   visibility,
   project_id,
-  invest_type,
+  // invest_type,
 }) {
   let navigate = useNavigate();
 
@@ -149,9 +149,9 @@ function ProjectCollect({
           <Typography variant="body2" color="secondary.light">
             Votre contribution vous sera intégralement remboursée si le projet
             n&apos;atteint pas 100% de son objectif. <br />
-            <br />
+            {/* <br />
             Type d&apos;investissement : &nbsp;
-            <span style={{ fontWeight: "bold" }}>{invest_type}</span>
+            <span style={{ fontWeight: "bold" }}>{invest_type}</span> */}
           </Typography>
         </CardContent>
 
